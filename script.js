3ab9fd3e106bed6efb7aa143e2a5f69305a92608
slash <minsuslash@gmail.com>
2023-01-12 21:47:36 +0900

    3

--- script.js
+++ script.js
@@ -12,6 +12,20 @@ function staticLoadPlaces() {
                lng: 126.918297,
            }
        },
+       {
+           name: 'Magnemite',
+           location: {
+               lat: 37.561498,
+               lng: 126.918329,
+           }
+       },
+       {
+           name: 'Magnemite',
+           location: {
+               lat: 37.561486,
+               lng: 126.918310,
+           }
+       },
    ];
 }
 
@@ -27,7 +41,7 @@ function renderPlaces(places) {
        model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
        model.setAttribute('rotation', '0 180 0');
        model.setAttribute('animation-mixer', '');
-       model.setAttribute('scale', '0.5 0.5 0.5');
+       model.setAttribute('scale', '1 1 1');
 
        model.addEventListener('loaded', () => {
            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
