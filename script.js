d63725b4dbf635140f1fee2ddaa042976417e6e5
slash <minsuslash@gmail.com>
2023-01-12 21:45:46 +0900

    2

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function staticLoadPlaces() {
        {
            name: 'Magnemite',
            location: {
-               lat: 44.496470,
-               lng: 11.320180,
+               lat: 37.561476,
+               lng: 126.918297,
            }
        },
    ];
